f7480826642e0fbeaa1114963e41b57f79428c72
daniel-lou18 <danielderudder@hotmail.com>
2024-05-24 15:16:21 +0200

    Mettre les composants clients SignUpForm et SignInForm à l'intérieur d'un élément Suspense

--- src/app/(shop)/login/page.tsx
+++ src/app/(shop)/login/page.tsx
@@ -1,88 +1,12 @@
-"use client";
+import { Suspense } from "react";
+import SignInForm from "./SignInForm";
 
-import Link from "next/link";
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import * as actions from "@/actions";
-import { auth } from "@/auth";
-import Profile from "./_components/Profile";
-import ButtonLogin from "./_components/ButtonLogin";
-import ButtonSubmit from "@/components/ui/ButtonSubmit";
-import { useFormState } from "react-dom";
-import FormFieldError from "@/components/ui/FormFieldError";
-import { paths } from "@/lib/paths";
-import InputField from "@/components/ui/InputField";
-import { useSearchParams } from "next/navigation";
-import { toast } from "sonner";
-
-function SignInForm() {
-  const [formState, action] = useFormState(actions.signInUser, {});
-  const searchParams = useSearchParams();
-
-  if (searchParams.get("success") === "signup") {
-    toast.success("Votre compté a été créé. Vous pouvez vous connecter.");
-  }
-
+async function SignInPage() {
   return (
-    <main className="flex flex-col items-center pt-32 w-full min-h-screen">
-      <form>
-        <Card className="mx-auto max-w-sm">
-          <CardHeader>
-            <CardTitle className="text-2xl">Connexion</CardTitle>
-            <CardDescription>
-              Saisissez votre email et votre mot de passe
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="grid gap-4">
-              <div className="grid gap-2">
-                <InputField name="email" variant="input" type="custom" />
-                <FormFieldError>
-                  {formState.errors?.email?.join(", ")}
-                </FormFieldError>
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <Link
-                    href="#"
-                    className="ml-auto inline-block text-sm underline"
-                  >
-                    Mot de passe oublié ?
-                  </Link>
-                </div>
-                <Input name="password" id="password" type="password" />
-                <FormFieldError>
-                  {formState.errors?.password?.join(", ")}
-                </FormFieldError>
-              </div>
-              <ButtonSubmit className="w-full" formAction={action}>
-                Login
-              </ButtonSubmit>
-              <div className="pointer-events-none opacity-50">
-                <ButtonLogin variant="outline" />
-              </div>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Vous n&apos;avez pas encore de compte professionnel ?{" "}
-              <Link href={paths.customerSignup()} className="underline">
-                Créer votre compte
-              </Link>
-            </div>
-            <Profile />
-          </CardContent>
-        </Card>
-      </form>
-    </main>
+    <Suspense>
+      <SignInForm />
+    </Suspense>
   );
 }
 
-export default SignInForm;
+export default SignInPage;
---
+++ src/app/(shop)/signup/SignUpForm.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import ButtonSubmit from "@/components/ui/ButtonSubmit";
+import InputField from "@/components/ui/InputField";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useFormState } from "react-dom";
+import * as actions from "@/actions";
+import FormFieldError from "@/components/ui/FormFieldError";
+import { toast } from "sonner";
+
+function SignUpForm() {
+  const [formState, action] = useFormState(actions.signUpUser, {});
+
+  if (formState.errors)
+    toast.error("Veuillez corriger les erreurs s'il vous plaît");
+
+  return (
+    <form action={action} className="mx-auto max-w-[59rem] flex flex-col">
+      <h2 className="text-xl font-semibold leading-7 text-gray-900">
+        Créer mon compte
+      </h2>
+      <p className="mt-1 text-sm leading-6 text-gray-600">
+        Veuillez saisir vos informations personnelles
+      </p>
+
+      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div className="sm:col-span-3">
+          <InputField name="firstName" variant="input" type="custom" />
+          <FormFieldError>
+            {formState.errors?.firstName?.join(", ")}
+          </FormFieldError>
+        </div>
+
+        <div className="sm:col-span-3">
+          <InputField name="lastName" variant="input" type="custom" />
+          <FormFieldError>
+            {formState.errors?.lastName?.join(", ")}
+          </FormFieldError>
+        </div>
+
+        <div className="sm:col-span-4">
+          <InputField name="email" variant="input" type="custom" />
+          <FormFieldError>{formState.errors?.email?.join(", ")}</FormFieldError>
+        </div>
+
+        <div className="sm:col-span-3">
+          <label
+            htmlFor="country"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Pays
+          </label>
+          <div className="mt-2">
+            <select
+              id="country"
+              name="country"
+              autoComplete="country-name"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+            >
+              <option>France</option>
+              <option>Belgique</option>
+              <option>Luxembourg</option>
+            </select>
+          </div>
+          <FormFieldError>
+            {formState.errors?.country?.join(", ")}
+          </FormFieldError>
+        </div>
+
+        <div className="col-span-full">
+          <InputField name="address" variant="input" type="custom" />
+          <FormFieldError>
+            {formState.errors?.address?.join(", ")}
+          </FormFieldError>
+        </div>
+
+        <div className="sm:col-span-2 sm:col-start-1">
+          <InputField name="city" variant="input" type="custom" />
+          <FormFieldError>{formState.errors?.city?.join(", ")}</FormFieldError>
+        </div>
+
+        <div className="sm:col-span-2">
+          <InputField name="state" variant="input" type="custom" />
+          <FormFieldError>{formState.errors?.state?.join(", ")}</FormFieldError>
+        </div>
+
+        <div className="sm:col-span-2">
+          <InputField name="zip" variant="input" type="custom" />
+          <FormFieldError>{formState.errors?.zip?.join(", ")}</FormFieldError>
+        </div>
+      </div>
+      <div className="items-center gap-2 flex mt-8 self-end">
+        <Button type="button" variant="outline" size="sm" asChild>
+          <Link href="/admin/products">Annuler </Link>
+        </Button>
+        <ButtonSubmit>Créer mon compte</ButtonSubmit>
+      </div>
+    </form>
+  );
+}
+
+export default SignUpForm;
