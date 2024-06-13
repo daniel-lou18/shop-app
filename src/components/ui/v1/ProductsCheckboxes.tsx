13dad6d829f1cc9c658762790923c26b0b89dfe2
daniel-lou18 <danielderudder@hotmail.com>
2024-06-13 17:31:29 +0200

    Corriger erreur

--- src/components/ui/v1/ProductsCheckboxes.tsx
+++ src/components/ui/v1/ProductsCheckboxes.tsx
@@ -1,28 +1,28 @@
-"use client";
+// "use client";
 
-import ProductsSelectSex from "../../../app/admin/products/_components/ProductsSelectSex";
-import ProductsCheckbox from "../../../app/admin/products/_components/ProductsCheckbox";
-import TableActions from "../../admin/TableActions";
+// import ProductsSelectSex from "../../../app/admin/products/_components/ProductsSelectSex";
+// import ProductsCheckbox from "../../../app/admin/products/_components/ProductsCheckbox";
+// import TableActions from "../../admin/TableActions";
 
-function ProductsCheckboxes({
-  brands,
-  categories,
-}: {
-  brands: string[];
-  categories: string[];
-}) {
-  return (
-    <div className="flex gap-4 flex-1 pb-4">
-      <div
-        className="flex items-center relative"
-        id="table-search-container"
-      ></div>
-      <ProductsSelectSex title="Collection" />
-      <ProductsCheckbox data={brands} type="brand" />
-      <ProductsCheckbox data={categories} type="category" />
-      <TableActions buttonText={`Ajouter produit`} />
-    </div>
-  );
-}
+// function ProductsCheckboxes({
+//   brands,
+//   categories,
+// }: {
+//   brands: string[];
+//   categories: string[];
+// }) {
+//   return (
+//     <div className="flex gap-4 flex-1 pb-4">
+//       <div
+//         className="flex items-center relative"
+//         id="table-search-container"
+//       ></div>
+//       <ProductsSelectSex title="Collection" />
+//       <ProductsCheckbox data={brands} type="brand" />
+//       <ProductsCheckbox data={categories} type="category" />
+//       <TableActions buttonText={`Ajouter produit`} />
+//     </div>
+//   );
+// }
 
-export default ProductsCheckboxes;
+// export default ProductsCheckboxes;
