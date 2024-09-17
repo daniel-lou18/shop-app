616c9b1d6705a6447407a5f73b152ea598e74393
daniel-lou18 <danielderudder@hotmail.com>
2024-09-17 16:43:40 +0200

    The search field results are not visible anymore when you click outside the input field

--- src/components/layout/search/Search.tsx
+++ src/components/layout/search/Search.tsx
@@ -2,12 +2,36 @@ import { Input } from "@/components/ui/input";
 import { Search as SearchIcon } from "lucide-react";
 import SearchResults from "./SearchResults";
 import { useSearchProducts } from "@/hooks/useSearchProducts";
+import { useEffect, useRef, useState } from "react";
 
 export default function SearchField() {
   const { error, products, query, setQuery } = useSearchProducts();
+  const [isVisible, setIsVisible] = useState(true);
+  const inputFieldRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleOutsideClick(e: MouseEvent) {
+      if (
+        inputFieldRef.current &&
+        !inputFieldRef.current.contains(e.target as Node)
+      ) {
+        setIsVisible(false);
+      }
+    }
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => document.removeEventListener("click", handleOutsideClick);
+  }, []);
+
+  useEffect(() => {
+    if (query) {
+      setIsVisible(true);
+    }
+  }, [query]);
 
   return (
-    <div className="relative ml-auto flex-1 md:grow-0">
+    <div ref={inputFieldRef} className="relative ml-auto flex-1 md:grow-0">
       <SearchIcon className="absolute left-2.5 top-3 h-4 w-4 text-muted-foreground" />
       <Input
         type="search"
@@ -18,7 +42,7 @@ export default function SearchField() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
-      <SearchResults results={products} />
+      {isVisible && <SearchResults results={products} />}
     </div>
   );
 }
