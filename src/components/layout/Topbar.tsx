327857271a4e3cab33e5d6c8fc7b254f9e6be60e
daniel-lou18 <danielderudder@hotmail.com>
2024-09-24 10:56:36 +0200

    Shorten promo message on mobile devices

--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -3,6 +3,7 @@
 import { useCart } from "@/context/cart-context";
 import { mapCartPriceToMessage } from "@/lib/helpers";
 import React from "react";
+import BaseComponent from "../ui/BaseComponent";
 
 function Topbar() {
   const { totalPrice } = useCart();
@@ -11,7 +12,12 @@ function Topbar() {
 
   return (
     <section className="text-white text-sm md:text-base bg-primary w-full h-10 flex gap-2 justify-center items-center">
-      <span>{message}</span>
+      <BaseComponent tag="span" className="hidden sm:inline">
+        {message}
+      </BaseComponent>
+      <BaseComponent tag="span" className="sm:hidden">
+        {message?.split("sur toute")[0]}
+      </BaseComponent>
     </section>
   );
 }
