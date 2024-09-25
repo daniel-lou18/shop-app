67971ab5dd4d07a16519fb8871146178b8650f1d
daniel-lou18 <danielderudder@hotmail.com>
2024-09-25 15:16:17 +0200

    Add test for ProductService and filtering logic

--- src/features/products/ProductService.ts
+++ src/features/products/ProductService.ts
@@ -1,12 +1,24 @@
 import { IProductRepository } from "./interface";
+import { Product } from "./Product";
 
 export class ProductService {
   constructor(private repo: IProductRepository) {}
 
-  async getMany(query: string) {
+  async getMany(
+    query: string,
+    filterFn?: (query: string, data: Product[]) => Product[]
+  ) {
     const data = await this.repo.findMany();
 
-    const filteredData = data
+    if (filterFn) {
+      return filterFn(query, data);
+    }
+
+    return this.filterData(query, data);
+  }
+
+  filterData(query: string, data: Product[]) {
+    return data
       .filter(
         (product) =>
           product.name.toLowerCase().includes(query.trim().toLowerCase()) ||
@@ -18,7 +30,5 @@ export class ProductService {
             .includes(query.trim().toLowerCase())
       )
       .slice(0, 10);
-
-    return filteredData;
   }
 }
--- src/features/products/__tests__/ProductService.test.ts
+++ src/features/products/__tests__/ProductService.test.ts
@@ -8,11 +8,86 @@ const mockProductRepository: IProductRepository = {
   ),
 };
 
-describe("ProductService class", () => {
-  test("initializes correctly", () => {
-    const productService = new ProductService(mockProductRepository);
+const mockData = [
+  {
+    id: "clxoylywc001bcxplmisp6e86",
+    name: "Jean en cotton bio",
+    size: "XS",
+    color: "blanc",
+    price: 332550,
+    image: "/sweat_blanc_1.jpg",
+    productName: "Jean en cotton bio",
+    brandName: "Levi's",
+    categoryName: "jeans",
+    sex: "homme" as const,
+  },
+  {
+    id: "clxoym0hh001ocxplmjoezzcd",
+    name: "Pull en cotton bio",
+    size: "XS",
+    color: "blanc",
+    price: 97950,
+    image: "/sweat_blanc_1.jpg",
+    productName: "Pull en cotton bio",
+    brandName: "Sandro",
+    categoryName: "pulls et gilets",
+    sex: "homme" as const,
+  },
+  {
+    id: "clxoym22o0021cxplvppetwou",
+    name: "Veste en cotton bio",
+    size: "XS",
+    color: "blanc",
+    price: 170800,
+    image: "/sweat_blanc_1.jpg",
+    productName: "Veste en cotton bio",
+    brandName: "Gucci",
+    categoryName: "vestes",
+    sex: "homme" as const,
+  },
+];
 
+const expectedFilteredResult = [
+  {
+    id: "clxoym0hh001ocxplmjoezzcd",
+    name: "Pull en cotton bio",
+    size: "XS",
+    color: "blanc",
+    price: 97950,
+    image: "/sweat_blanc_1.jpg",
+    productName: "Pull en cotton bio",
+    brandName: "Sandro",
+    categoryName: "pulls et gilets",
+    sex: "homme" as const,
+  },
+];
+
+const mockProductRepositoryFilter: IProductRepository = {
+  findMany: jest.fn(() => new Promise((resolve) => resolve(mockData))),
+};
+
+describe("ProductService class", () => {
+  let productService = new ProductService(mockProductRepository);
+
+  test("initializes correctly", () => {
     expect(productService).toBeInstanceOf(ProductService);
     expect(productService.getMany).toBeDefined();
+  });
+
+  test("it executes the findMany method on the repository when the getMany method is called", async () => {
+    await productService.getMany("");
+    expect(mockProductRepository.findMany).toHaveBeenCalled();
+  });
+
+  test("it returns the correct value if the query is an empty string", async () => {
+    const result = await productService.getMany("");
+    expect(result).toEqual(testVariantsWithProductMenFlattened);
+  });
+
+  test("it returns the correct filtered values", async () => {
+    productService = new ProductService(mockProductRepositoryFilter);
+
+    const result = await productService.getMany("pull");
+    expect(result).toEqual(expectedFilteredResult);
   });
 });
